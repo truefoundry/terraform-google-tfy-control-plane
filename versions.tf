0edd9e559770b0172952aed7449e747e7aff5a76
Vedant Pareek <dunefro@gmail.com>
2024-11-05 01:18:56 +0530

    Added version for control plane

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.85.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
